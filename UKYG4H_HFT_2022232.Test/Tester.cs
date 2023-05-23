49831081b236eefb8a95de4ab72f415fcac25111
kobori.bence <kobori.bence@stud.uni-obuda.hu>
2023-05-23 16:58:28 +0200

    Tester class nuget, módosítva

---
+++ UKYG4H_HFT_2022232.Test/Tester.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using System;
+using UKYG4H_HFT_2022232.Logic;
+using UKYG4H_HFT_2022232.Models;
+using UKYG4H_HFT_2022232.Repository;
+using NUnit;
+
+namespace UKYG4H_HFT_2022232.Test
+{
+    public class Tester
+    {
+    }
+}
